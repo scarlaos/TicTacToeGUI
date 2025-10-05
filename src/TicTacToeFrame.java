b69980fc40c53e5469adda307bb36533e6b32cb8
scarlaos <scarlaos@mail.uc.edu>
2025-10-04 22:12:49 -0400

    i think its done?

--- src/TicTacToeFrame.java
+++ src/TicTacToeFrame.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TicTacToeFrame extends JFrame implements ActionListener {
-    JFrame ticTacToe = new JFrame("Tic-Tac-Toe Game");
+
 
 
     //panels
@@ -31,6 +31,8 @@ public class TicTacToeFrame extends JFrame implements ActionListener {
     int count = 0;
     int XplayerWins =0;
     int OplayerWins = 0;
+    int tieWins = 0;
+    int score = 0;
     boolean win = false;
     String player;
 
@@ -60,9 +62,10 @@ public class TicTacToeFrame extends JFrame implements ActionListener {
         }
 
         createButtonPanel(); // this will hold the reset quit and score buttons
-        ticTacToe.add(TTTBoard,BorderLayout.CENTER);
-        ticTacToe.add(buttonPanel,BorderLayout.SOUTH);
+        add(TTTBoard,BorderLayout.CENTER);
+        add(buttonPanel,BorderLayout.SOUTH);
 
+        setSize(400,400);
         setVisible(true);
     }
 
@@ -90,54 +93,73 @@ public class TicTacToeFrame extends JFrame implements ActionListener {
         *    button.setEnabled(true);
         *}
         */ //might need this here, I havent gotten to that
-        checkWin();
+        if(checkWin()){
+            win = true;
+            if(player.equals("X")){
+                XplayerWins ++;
+            }else {
+                OplayerWins ++;
+            }
+            //game is won
+            int playAgain = JOptionPane.showConfirmDialog(this,"Player" + player + " won!\nWould you like to play again?","The game is over.",
+                    JOptionPane.YES_NO_OPTION);
+
+            if(playAgain == JOptionPane.YES_OPTION){
+                reset();
+            }else{
+                System.exit(0);
+            }
+
+            return;
+        }
+
+
+
+        //game results in a tie
+        if(count == 9 &&!win){
+            win = true;
+            tieWins ++;
+            int playAgain = JOptionPane.showConfirmDialog(this,"The game resulted in a tie!\nWould you like to play again?",
+                "Game is Tied!", JOptionPane.YES_NO_OPTION);
+            if(playAgain == JOptionPane.YES_OPTION){
+                reset();
+            }
+            else{
+                System.exit(0);
+            }
+
+            return;
+        }
+
+
 
     }
 
-    public void checkWin(){
+    public boolean checkWin(){
         JButton[] allButtons = {buttonOne,buttonTwo,buttonThree,buttonFour,buttonFive,buttonSix,buttonSeven,buttonEight,buttonNine};
 
-        //verticals
-        if(buttonOne.getText() == buttonFour.getText() && buttonFour.getText() == buttonSeven.getText()){
-            win = true;
+        // verticals
+        if(!buttonOne.getText().equals("") && buttonOne.getText().equals(buttonFour.getText()) && buttonFour.getText().equals(buttonSeven.getText())) return true;
+        if(!buttonTwo.getText().equals("") && buttonTwo.getText().equals(buttonFive.getText()) && buttonFive.getText().equals(buttonEight.getText())) return true;
+        if(!buttonThree.getText().equals("") && buttonThree.getText().equals(buttonSix.getText()) && buttonSix.getText().equals(buttonNine.getText())) return true;
 
-        }
-        else if(buttonTwo.getText() == buttonFive.getText() && buttonFive.getText() == buttonEight.getText()){
-            win = true;
-        }
-        else if(buttonThree.getText() == buttonSix.getText() && buttonSix.getText() == buttonNine.getText()){
-            win = true;
-        }
+        // horizontals
+        if(!buttonOne.getText().equals("") && buttonOne.getText().equals(buttonTwo.getText()) && buttonTwo.getText().equals(buttonThree.getText())) return true;
+        if(!buttonFour.getText().equals("") && buttonFour.getText().equals(buttonFive.getText()) && buttonFive.getText().equals(buttonSix.getText())) return true;
+        if(!buttonSeven.getText().equals("") && buttonSeven.getText().equals(buttonEight.getText()) && buttonEight.getText().equals(buttonNine.getText())) return true;
 
-        //horizontals
-        else if(buttonOne.getText() == buttonTwo.getText() && buttonTwo.getText() == buttonThree.getText()){
-            win = true;
-        }
-        else if(buttonFour.getText() == buttonFive.getText() && buttonFive.getText() == buttonSix.getText()){
-            win = true;
-        }
-        else if(buttonSeven.getText() == buttonEight.getText() && buttonEight.getText() == buttonNine.getText()){
-            win = true;
-        }
+        // diagonals
+        if(!buttonOne.getText().equals("") && buttonOne.getText().equals(buttonFive.getText()) && buttonFive.getText().equals(buttonNine.getText())) return true;
+        if(!buttonThree.getText().equals("") && buttonThree.getText().equals(buttonFive.getText()) && buttonFive.getText().equals(buttonSeven.getText())) return true;
 
-        //diagonals
-
-        else if(buttonOne.getText() == buttonFive.getText() && buttonFive.getText() == buttonNine.getText()){
-            win = true;
-        }
-        else if(buttonThree.getText() == buttonFive.getText() && buttonFive.getText() == buttonSeven.getText()){
-            win = true;
-        }
-        else{
-            win = false;
-        }
+        return false;
     }
 
     public void createButtonPanel(){
         buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(1,3));
 
-        buttonPanel.add(resetButton);
+        // add this if you're awake olivia: buttonPanel.add(resetButton);
         resetButton.addActionListener((ActionEvent e)-> {
             int reply = JOptionPane.showConfirmDialog(null,"Are you sure you want to reset the board?","Confirmation: ",JOptionPane.YES_NO_OPTION);
 
@@ -145,6 +167,18 @@ public class TicTacToeFrame extends JFrame implements ActionListener {
                 reset();
                 count = 0;
             }
+        });
+
+        buttonPanel.add(resetButton);
+        buttonPanel.add(quitButton);
+        quitButton.addActionListener((ActionEvent e)-> {
+            System.exit(0);
+        });
+
+        buttonPanel.add(scoreButton); //scoreboard
+        scoreButton.addActionListener((ActionEvent e)-> {
+            JOptionPane.showMessageDialog(this,"Player X Wins: " + XplayerWins + "\nPlayer 0 Wins: " + OplayerWins + "\nTies: " + tieWins,
+                    "ScoreBoard", JOptionPane.INFORMATION_MESSAGE);
         });
     }
 
@@ -155,6 +189,7 @@ public class TicTacToeFrame extends JFrame implements ActionListener {
             button.setText("");
             button.setEnabled(true);
         }win = false;
+        count = 0;
     }
 
 }
--- src/TicTacToeRunner.java
+++ src/TicTacToeRunner.java
@@ -3,5 +3,9 @@ import javax.swing.*;
 public class TicTacToeRunner {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Tic-Tac-Toe");
+
+        SwingUtilities.invokeLater(() ->{
+            new TicTacToeFrame();
+        });
     }
 }
